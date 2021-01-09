d14ff6752cdd255ab5d7123e8cc1e7db04998e01
James Tejada <james.e.tejada@gmail.com>
2021-01-09 04:55:20 -0800

    initial commit

---
+++ run.py
@@ -0,0 +1,20 @@
+
+
+# 1. Get File
+#   a. log in to ftp or scrape to get download link
+#       - maybe scrape to get some files. We'll see. 
+#   b. determine which program to download (maybe by modified date)
+#   c. Download file
+
+# 2. process file
+#   a. create mp3 version of promo
+#       - lame.exe?
+#       - mutagen?
+#   b. rename .wav for ingest into DAD
+#       - cut number
+#       - title
+#       - etc
+
+# 3. copy to destinations
+#   a. DAD DROPBOX
+#   b. FFA Continuity Folder.
