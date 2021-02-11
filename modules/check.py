b086315c93a8a8221b55bcfc45b07c2c3ba77905
James Tejada <james.e.tejada@gmail.com>
2021-02-10 16:35:35 -0800

    tweaked colors again

--- modules/check.py
+++ modules/check.py
@@ -8,7 +8,7 @@ class Check_BASE(process.Reveal):
         exist_dict = self._which_exist()
         for segment in exist_dict:
             color = Fore.GREEN if exist_dict.get(segment) else Fore.RED
-            style = Style.BRIGHT if exist_dict.get(segment) else Style.NORMAL
+            style = Style.BRIGHT if exist_dict.get(segment) else Style.DIM
             print(color, style, segment.replace('_', ' ').title(), Style.RESET_ALL)
     
     def _which_exist(self):
@@ -19,19 +19,15 @@ class Check_BASE(process.Reveal):
     
     def __str__(self):
         return self.__class__.__name__.replace('_', ' ')
-    
+
 
 class Reveal(Check_BASE, process.Reveal): ...
-
 class Latino_USA(Check_BASE, process.Latino_USA): ...
-
 class Says_You(Check_BASE, process.Says_You): ...
-
 class The_Moth(Check_BASE, process.The_Moth): ...
-
 class Snap_Judgment(Check_BASE, process.Snap_Judgment): ...
-
 class This_American_Life(Check_BASE, process.This_American_Life): ...
+
 
 CHECK_SHOWS = [
     Reveal, 
@@ -41,6 +37,7 @@ CHECK_SHOWS = [
     Snap_Judgment,
     This_American_Life
 ]
+
 
 def check_all():
     for show_class in CHECK_SHOWS:
