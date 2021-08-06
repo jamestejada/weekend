aaacc8996d00dacdbb07ce4275db62fa61855fc6
James Tejada <james.e.tejada@gmail.com>
2021-08-06 14:54:46 -0700

    moar testing! still building process class.

--- modules/process.py
+++ modules/process.py
@@ -3,50 +3,63 @@ from modules.data import DATA_LIST, Show
 from modules.settings import PATHS
 from datetime import datetime, timedelta
 
+class Audio:
+    def __init__(
+        self, sample_rate:int=44100, 
+        target_level:float=-24.0, true_peak:float=-3.0, bitrate:str='256k'
+        ) -> None:
+        pass
 
-class Process:
+
+class Process(Audio):
     LOCAL_PATH = PATHS.LOCAL_PATH
     FOR_DROPBOX = PATHS.FOR_DROPBOX
     FOR_FFA = PATHS.FOR_FFA
 
     def __init__(
-        self, show_data:Show, process_list:list=None, sample_rate:int=44100,
-        target_level:float=-24.0, true_peak:float=-3.0,
-        bitrate:str='256k', threading:bool=False
+        self, show_data:Show, process_list:list=None, threading:bool=False, _local_path=None,
+        _destination_path=None
         ) -> None:
         
+        self.local_path = _local_path or self.LOCAL_PATH
+        self.destination_path  = _destination_path or self.FOR_DROPBOX
+
+        self.process_list = process_list
         self.show_string = show_data.show_name
         self.show_match_strings = show_data.show_match
         self.max_number_of_files = show_data.number_of_files
+        self.segment_match = show_data.segment_match
+        self.cut_numbers = show_data.cut_numbers
 
         self.air_days_string = self.get_days_string(show_data.air_days)
-        self.process_list = process_list
 
         self.file_list = self.get_file_list(process_list=self.process_list)
+        self.source_paths = self.get_source_paths()
+        self.destination_paths = self.get_destination_paths()
 
 
-    def get_file_list(self, process_list=None, _local_path=None):
+
+    def get_file_list(self, process_list=None):
         """Returns the list to be processed. Either:
             1. the matching show files in LOCAL_PATH or
             2. the process_list passed in of files that have just been downloaded.
         """
-        local_path = _local_path or self.LOCAL_PATH
-        local_dir_list = self._local_dir_files(local_path)
-        downloaded_list = self._get_downloaded_file_list(local_path, process_list=process_list)
+        local_dir_list = self._local_dir_files()
+        downloaded_list = self._get_downloaded_file_list(process_list=process_list)
         return downloaded_list or local_dir_list
 
-    def _get_downloaded_file_list(self, local_path, process_list=None) -> list:
+    def _get_downloaded_file_list(self, process_list=None) -> list:
         if not process_list:
             return []
         return [
-            local_path.joinpath(file_path.name)
+            self.local_path.joinpath(file_path.name)
             for file_path in process_list
             if self.match_show(file_path.name)
         ]
 
-    def _local_dir_files(self, local_path) -> list:
+    def _local_dir_files(self) -> list:
         directory_list = [
-            file_path for file_path in local_path.iterdir()
+            file_path for file_path in self.local_path.iterdir()
             if self.match_show(file_path.name)
         ]
         try:
@@ -78,3 +91,48 @@ class Process:
             for day in air_weekday_list
         ]
         return ' and '.join(air_date_list)
+    
+    def get_source_paths(self):
+        """Returns a dictionary with segment name as key
+        and the local file path as a value
+        EXAMPLE:
+        {
+            'billboard': Path('./weekend/files/downloads/RevealWk_270_SGMT01.wav)',
+            'segment_a': Path('./weekend/files/downloads/RevealWk_270_SGMT03.wav)',
+            'segment_b': Path('./weekend/files/downloads/RevealWk_270_SGMT05.wav)',
+            'segment_c': Path('./weekend/files/downloads/RevealWk_270_SGMT07.wav)'
+        }
+        """
+        return {
+            self.segment_match.get(match_string): file_path
+            for file_path in self.file_list
+            for match_string in self.segment_match.keys()
+                if match_string in file_path.name
+        }
+    
+    def get_destination_paths(self):
+        """Returns dictionary of destination paths 
+        {
+            'billboard': Path('./weekend/files/for_dropbox/RevealWk_270_SGMT01.wav'),
+            'segment_a': Path('./weekend/files/for_dropbox/RevealWk_270_SGMT03.wav'),
+            'segment_b': Path('./weekend/files/for_dropbox/RevealWk_270_SGMT05.wav'),
+            'segment_c': Path('./weekend/files/for_dropbox/RevealWk_270_SGMT05.wav')
+        }
+        """
+        return {
+            segment_name: self._get_dropbox_path(
+                segment_name=segment_name, extension=file_path.suffix
+                )
+            for segment_name, file_path in self.source_paths.items()
+        }
+
+    def _get_dropbox_path(self, segment_name, extension='.wav'):
+        """ Returns the destination path for DROPBOX files.
+        """
+        segment_string = segment_name.replace('_', ' ').upper()
+        return self.destination_path.joinpath(
+            f'{self.cut_numbers.get(segment_name)}_{self.show_string} '
+            + f'{segment_string} {self.air_days_string}{extension}'
+            )
+    
+
--- tests/test_process.py
+++ tests/test_process.py
@@ -5,6 +5,77 @@ from modules.data import Show
 from datetime import datetime, timedelta
 
 
+FAKE_PATHS = [
+            '/fakedir/weekend/files/downloads/14161.wav',
+            '/fakedir/weekend/files/downloads/35233.wav',
+            '/fakedir/weekend/files/downloads/35245.wav',
+            '/fakedir/weekend/files/downloads/cached_hashes.json',
+            '/fakedir/weekend/files/downloads/THEMOTH_445_PROM01.wav',
+            '/fakedir/weekend/files/downloads/THEMOTH_445_SGMT01.wav',
+            '/fakedir/weekend/files/downloads/ThisAmer_366_PROM01.wav',
+            '/fakedir/weekend/files/downloads/ThisAmer_366_PROM02.wav',
+            '/fakedir/weekend/files/downloads/test_bb32.wav',
+            '/fakedir/weekend/files/downloads/test_6542.wav',
+            '/fakedir/weekend/files/downloads/variable_test_.wav'
+        ]
+
+AIR_DAYS_NUMBERS = 6
+
+@pytest.fixture
+def air_day_string():
+    today = datetime.today()
+    return (today - timedelta(days=today.weekday()) + timedelta(days=6)).strftime('%b %-d')\
+
+
+class Mock_File_Path:
+    def __init__(self, path_string: str) -> None:
+        self.path_string = path_string    
+
+    @property
+    def name(self):
+        return self.path_string.split('/')[-1]
+    
+    @property
+    def stem(self):
+        return self.name.split('.')[0]
+    
+    @property
+    def suffix(self):
+        extension = self.name.split('.')[-1]
+        return f'.{extension}'
+
+    def __str__(self) -> str:
+        return str(self.path_string)
+
+
+class Mock_Dirs:
+    FAKE_PATHS = FAKE_PATHS
+    def __init__(self, path_list:list=None, dir_path_string:str=None) -> None:
+        self.fake_path_strings = path_list or self.FAKE_PATHS
+        self.dir_path_string = dir_path_string or '/fakedir/weekend/files/downloads/'
+
+    def iterdir(self):
+        for each_path in self.fake_path_strings:
+            yield Mock_File_Path(each_path)
+
+    def joinpath(self, *args):
+        path_list = self.dir_path_string.split('/')
+        path_list = [each_path for each_path in path_list if each_path != '']
+        for arg in args:
+            path_list.append(arg)
+        return Mock_File_Path('/' + '/'.join(path_list))
+
+
+@pytest.fixture
+def mock_dropbox_dir():
+    return Mock_Dirs(dir_path_string='/fakedir/weekend/files/for_dropbox/')
+
+
+@pytest.fixture
+def mock_local_dir():
+    return Mock_Dirs()
+
+
 @pytest.fixture
 def test_show():
     return Show(
@@ -14,13 +85,27 @@ def test_show():
         remote_dir='test_',
         add_time_target=3060,
         first_day_offset_offset=1,
-        air_days=[6]
+        air_days=[AIR_DAYS_NUMBERS],
+        segment_match={
+            'bb32': 'billboard',
+            '_6542': 'segment_a',
+            'variable_': 'segment_b'
+        },
+        cut_numbers={
+            'billboard': '31337',
+            'segment_a': '01531',
+            'segment_b': '01492'
+        }
     )
 
 
 @pytest.fixture
-def processor(test_show):
-    return Process(test_show)
+def processor(test_show, mock_local_dir, mock_dropbox_dir):
+    return Process(
+        test_show,
+        _local_path=mock_local_dir, 
+    _destination_path=mock_dropbox_dir
+    )
 
 
 @pytest.mark.parametrize('air_weekday_list', [[5,6], [6,7], [6], [7]])
@@ -47,67 +132,47 @@ def test_Process_match_show_false(processor, file_name):
     assert not processor.match_show(file_name)
 
 
-FAKE_PATHS = [
-            '/fakedir/weekend/files/downloads/14161.wav',
-            '/fakedir/weekend/files/downloads/35233.wav',
-            '/fakedir/weekend/files/downloads/35245.wav',
-            '/fakedir/weekend/files/downloads/cached_hashes.json',
-            '/fakedir/weekend/files/downloads/THEMOTH_445_PROM01.wav',
-            '/fakedir/weekend/files/downloads/THEMOTH_445_SGMT01.wav',
-            '/fakedir/weekend/files/downloads/ThisAmer_366_PROM01.wav',
-            '/fakedir/weekend/files/downloads/ThisAmer_366_PROM02.wav',
-            '/fakedir/weekend/files/downloads/test_.wav',
-            '/fakedir/weekend/files/downloads/test_6542.wav',
-            '/fakedir/weekend/files/downloads/variable_test_.wav'
-        ]
-
-
-class Mock_File_Path:
-    def __init__(self, path_string: str) -> None:
-        self.path_string = path_string    
-
-    @property
-    def name(self):
-        return self.path_string.split('/')[-1]
-    
-    @property
-    def stem(self):
-        return self.name.split('.')[0]
-
-    def __str__(self) -> str:
-        return str(self.path_string)
-
-
-class Mock_Dirs:
-    FAKE_PATHS = FAKE_PATHS
-    def __init__(self, path_list:list=None) -> None:
-        self.fake_path_strings = path_list or self.FAKE_PATHS
-        self.dir_path_string = '/fakedir/weekend/files/downloads/'
-
-    def iterdir(self):
-        for each_path in self.fake_path_strings:
-            yield Mock_File_Path(each_path)
-
-    def joinpath(self, *args):
-        path_list = self.dir_path_string.split('/')
-        path_list = [each_path for each_path in path_list if each_path != '']
-        for arg in args:
-            path_list.append(arg)
-        return Mock_File_Path('/' + '/'.join(path_list))
-
-
-def test_Process_get_file_list_without_process_list(processor):
-    expected_show_list = [show_file for show_file in Mock_Dirs().iterdir() if processor.match_show(show_file.name)]
-    fake_pathlib_dir = Mock_Dirs()
-    actual_show_list = processor.get_file_list(_local_path=fake_pathlib_dir)
+def test_Process_get_file_list_without_process_list(processor, mock_local_dir):
+    expected_show_list = [show_file for show_file in mock_local_dir.iterdir() if processor.match_show(show_file.name)]
+    actual_show_list = processor.get_file_list()
     for fake, actual in zip(expected_show_list, actual_show_list):
         assert str(fake) == str(actual)
 
 
-def test_Process_get_file_list_with_process_list(processor):
-    fake_process_list = [file_path for file_path in Mock_Dirs().iterdir() if processor.match_show(file_path.name)]
-    fake_pathlib_dir = Mock_Dirs()
+def test_Process_get_file_list_with_process_list(processor, mock_local_dir, test_show):
+    fake_process_list = [
+        file_path for file_path in mock_local_dir.iterdir()
+        if any(
+            bool(show_match_str in file_path.name) 
+            for show_match_str in test_show.show_match
+            )
+        ]
     expected = [show_file for show_file in fake_process_list if processor.match_show(show_file.name)]
-    actual = processor.get_file_list(process_list=fake_process_list, _local_path=fake_pathlib_dir)
+    actual = processor.get_file_list(process_list=fake_process_list)
     for actual_file, expected_file in zip(actual, expected):
         assert str(actual_file) == str(expected_file)
+
+
+def test_Process_get_source_paths(processor):
+    expected = {
+            'billboard': Mock_File_Path('/fakedir/weekend/files/downloads/test_bb32.wav'),
+            'segment_a': Mock_File_Path('/fakedir/weekend/files/downloads/test_6542.wav'),
+            'segment_b': Mock_File_Path('/fakedir/weekend/files/downloads/variable_test_.wav')
+    }
+    actual = processor.get_source_paths()
+    assert actual.keys() == expected.keys()
+    for key in expected.keys():
+        assert str(actual.get(key)) == str(expected.get(key))
+
+
+def test_Process_get_destination_paths(processor, air_day_string):
+
+    expected = {
+            'billboard': Mock_File_Path(f'/fakedir/weekend/files/for_dropbox/31337_Test Show BILLBOARD {air_day_string}.wav'),
+            'segment_a': Mock_File_Path(f'/fakedir/weekend/files/for_dropbox/01531_Test Show SEGMENT A {air_day_string}.wav'),
+            'segment_b': Mock_File_Path(f'/fakedir/weekend/files/for_dropbox/01492_Test Show SEGMENT B {air_day_string}.wav')
+    }
+    actual = processor.get_destination_paths()
+    assert actual.keys() == expected.keys()
+    for key in expected.keys():
+        assert str(actual.get(key)) == str(expected.get(key))
